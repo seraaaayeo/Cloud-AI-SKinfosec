e1c48d92fe9a135c5340c4fe4119b9e849788b7f
seraaaayeo <bluemoon18ds@gmail.com>
2020-07-13 09:38:49 +0900

    Fixed untracted files: files in test_ai_list dir

--- week2/day5/test_ai_list/Backend/delete_data.py
+++
@@ -1,8 +0,0 @@
-'''
-@function: delete data
-'''
-def ai_remove(name, ai_dict):
-    for i in ai_dict:
-        if(i["name"] == name):
-            del ai_dict[ai_dict.index(i)]
-    return print(f"수강생 {name}의 정보가 삭제되었습니다.")
